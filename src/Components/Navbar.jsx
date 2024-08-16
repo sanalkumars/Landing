41bd0ad5b2aad29ff0bb8a900ca59f31804d3f21
sanalkumars <sanalskumar126@gmail.com>
2024-08-16 11:33:45 +0530

    navbar and mobilenavbar finished

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,14 @@
+import { Menu, X } from "lucide-react";
 import logo from "../assets/logo.png"
 import { navItems } from "../constants";
+import { useState } from "react";
 const Navbar = () => {
+    const [mobileDraw, setmobileDraw] = useState(false);
+
+    const toggleNavbar = ()=>{
+        setmobileDraw(!mobileDraw);
+    }
+
   return (
     <nav className="sticky top-0 z-50 py-4 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
@@ -25,7 +33,31 @@ const Navbar = () => {
                        py-2 rounded-md">Create An Account
                     </a>
                 </div>
+                <div className="lg:hidden md:flex flex-col justify-end">
+                    <button onClick={toggleNavbar}>
+                        {mobileDraw ? <X/> : <Menu/>}
+                    </button>
+
+                </div>
             </div>
+            { mobileDraw && (
+                <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                    <ul>
+                       { navItems.map((item , index)=>(
+                            <li key={index} className="py-4">
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className=" flex  space-x-6">
+                        <a href="#" className="py-2 px-3 border rounded-md">Sign-In</a>
+                        <a href="#" className="bg-gradient-to-r from-orange-500 to-purple-800 px-3 py-2 rounded-md">
+                            Create An Account
+                    </a>
+                </div>
+                </div>
+            )
+            }
         </div>
     </nav>
   )
